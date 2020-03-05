84763ce79643d6e0d4984894973b93e79ed9a0d2
Orson Saint Fleur <orsonsaintfleurgmail.com>
2020-03-05 10:01:37 -0500

    12

--- js/problemTwelve.js
+++ js/problemTwelve.js
@@ -1,1 +1,4 @@
-
+// Write a function to split a string and convert it into an array of words. For example:
+String.prototype.isUpperCase = function() {
+  return this.toString() === this.toUpperCase()
+}

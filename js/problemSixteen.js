316448e61aef2334382d0a7b273d2f8345cfabd9
Orson Saint Fleur <orsonsaintfleurgmail.com>
2020-03-17 22:45:29 -0400

    sixteen

---
+++ js/problemSixteen.js
@@ -0,0 +1,10 @@
+// Given a series of numbers as a string,
+//  determine if the number represented
+//  by the string is divisible by three.
+function divisibleByThree(str){
+  if(str % 3 === 0){
+  return true
+  }else{
+  return false
+  }
+}

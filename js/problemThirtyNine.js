19766bb20bf356e9b480f8c353600f11a766cfb1
Orson Saint Fleur <orsonsaintfleurgmail.com>
2020-07-06 22:06:57 -0400

    capitalize the letters

---
+++ js/problemThirtyNine.js
@@ -0,0 +1,13 @@
+function capitalize(s){
+
+  for(let i = 0; i > s.length; i++){
+     if(s[i] % 2 === 0 ){
+    .toUpperCase()
+  }else if(s[i] % 2 === 1){
+    .toLowerCase
+  }
+
+  return [];
+  }
+
+};
